6ca847336551ea0e15f22bd19f5f9850fb6aa9a6
Joe Fleming <joe.fleming@gmail.com>
2014-10-22 17:19:14 -0700

    distill query_input validate into es service

--- src/kibana/services/es.js
+++ src/kibana/services/es.js
@@ -1,7 +1,9 @@
 define(function (require) {
+  var _ = require('lodash');
   require('elasticsearch');
 
   var es; // share the client amoungst all apps
+  var request = {};
   require('modules')
     .get('kibana', ['elasticsearch', 'kibana/config'])
     .service('es', function (esFactory, configFile, $q) {
@@ -12,6 +14,30 @@ define(function (require) {
         log: 'info',
         requestTimeout: 60000
       });
+
+      es.validateQuery = function (query, index) {
+        var type;
+
+        // use default index if one is not passed in
+        if (index == null) {
+          useDefaults();
+        }
+
+        return es.indices.validateQuery({
+          index: index,
+          type: type,
+          explain: true,
+          ignoreUnavailable: true,
+          body: {
+            query: query || { match_all: {} }
+          }
+        });
+
+        function useDefaults() {
+          index = configFile.kibanaIndex;
+          type = '__kibanaQueryValidator';
+        }
+      };
 
       return es;
     });
