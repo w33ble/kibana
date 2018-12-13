a7c7f10f3b243701863c450735310da09e46eca1
spalger <email@spalger.com>
2018-12-13 10:12:40 -0800

    [canvas/plugins_build] create shared module directory for canvas plugins

--- x-pack/plugins/canvas/tasks/helpers/import_whitelist_plugin.js
+++ x-pack/plugins/canvas/tasks/helpers/import_whitelist_plugin.js
@@ -10,24 +10,40 @@ import isPathInside from 'path-is-inside';
 
 const KIBANA_ROOT = resolve(__dirname, '../../../../..');
 
+function matchesSomeCondition(pathsOrRegexps, path) {
+  return pathsOrRegexps.some(pathOrRegexp => {
+    if (pathOrRegexp instanceof RegExp) {
+      return pathOrRegexp.test(path);
+    }
+
+    return isPathInside(path, pathOrRegexp);
+  });
+}
+
 export class ImportWhitelistPlugin {
-  constructor({ whitelist }) {
+  constructor({ from, whitelist }) {
+    this.from = from;
     this.whitelist = whitelist;
   }
 
   apply(resolver) {
     resolver.hooks.file.tap('ImportWhitelistPlugin', request => {
-      const isAllowed = this.whitelist.some(pathOrRegexp => {
-        if (pathOrRegexp instanceof RegExp) {
-          return pathOrRegexp.test(request.path);
-        }
-        return isPathInside(request.path, pathOrRegexp);
-      });
+      if (!request.context || !request.context.issuer) {
+        // ignore internal requests that don't have an issuer
+        return;
+      }
 
-      if (!isAllowed) {
+      if (!matchesSomeCondition([this.from], request.context.issuer)) {
+        // request is not filtered by this whitelist unless it comes
+        // from a module matching the from conditions
+        return;
+      }
+
+      if (!matchesSomeCondition(this.whitelist, request.path)) {
         throw new Error(
-          `Attempted to import "${chalk.cyan(relative(KIBANA_ROOT, request.path))}". ` +
-            `Only node_modules and packages can be imported from outside of the "canvas_plugin_src" directory.`
+          `Attempted to import "${chalk.yellow(relative(KIBANA_ROOT, request.path))}" which ` +
+            `is not in the whitelist for the ${chalk.cyan(relative(KIBANA_ROOT, this.from))} ` +
+            `directory.`
         );
       }
     });
